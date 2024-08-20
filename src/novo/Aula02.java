73d4b75f8031f0d67afe7513ed0c95410ce32032
JOÃO ANTÔNIO MARTINS TAVEIRA <20211012000202@pucgo.edu.br>
2024-08-19 21:34:20 -0300

    feat: starting new activity for second class

---
+++ src/novo/Aula02.java
@@ -0,0 +1,12 @@
+package novo;
+
+import javax.swing.*;
+
+public class Aula02 {
+
+    private JButton button1;
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JFormattedTextField formattedTextField1;
+    private JFormattedTextField formattedTextField2;
+}
